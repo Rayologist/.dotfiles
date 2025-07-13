8f6f0f6496f067510a19a6d7b733b2e9a27a97ca
Bowei Chen <bwchen.dev@gmail.com>
2025-07-13 21:47:56 +0800

    Remove unused class declaration text objects from ECMAScript queries

--- nvim/after/queries/ecma/textobjects.scm
+++ nvim/after/queries/ecma/textobjects.scm
@@ -3,18 +3,3 @@
   (pair
     key: (_) @entry.lhs
     value: (_) @entry.inner @entry.rhs) @entry.outer)
-
-(class_declaration
-  body: (class_body)) @class.outer
-
-(class_declaration
-  body: (class_body
-    .
-    "{"
-    .
-    (_) @_start @_end
-    (_)? @_end
-    .
-    "}"
-    (#make-range! "class.inner" @_start @_end)))
-
