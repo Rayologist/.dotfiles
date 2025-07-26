b6aab2994d732d57b64bf06fe0044921e1bd1915
Bowei Chen <bwchen.dev@gmail.com>
2025-07-27 00:32:49 +0800

    Refactor Harpoon plugin configuration: comment out unused Telescope integration code for improved clarity and maintainability.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -14,26 +14,26 @@ return {
     local harpoon = require("harpoon")
 
     -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local make_entry = require("telescope.make_entry")
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require("telescope.pickers")
-        .new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-            entry_maker = make_entry.gen_from_file(),
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        })
-        :find()
-    end
+    -- local conf = require("telescope.config").values
+    -- local make_entry = require("telescope.make_entry")
+    -- local function toggle_telescope(harpoon_files)
+    --   local file_paths = {}
+    --   for _, item in ipairs(harpoon_files.items) do
+    --     table.insert(file_paths, item.value)
+    --   end
+    --
+    --   require("telescope.pickers")
+    --     .new({}, {
+    --       prompt_title = "Harpoon",
+    --       finder = require("telescope.finders").new_table({
+    --         results = file_paths,
+    --         entry_maker = make_entry.gen_from_file(),
+    --       }),
+    --       previewer = conf.file_previewer({}),
+    --       sorter = conf.generic_sorter({}),
+    --     })
+    --     :find()
+    -- end
     local keys = {
       {
         "<leader>ah",
