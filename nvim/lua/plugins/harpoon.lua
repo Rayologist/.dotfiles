32ca8bfff2056814e1288d2d5e2deaf4d8a77340
Bowei Chen <bwchen.dev@gmail.com>
2025-07-26 23:55:51 +0800

    Enhance Harpoon plugin configuration: add custom keybindings, integrate Telescope for file selection, and improve menu options with save on toggle setting.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,21 +1,66 @@
--- local harpoon = require("harpoon")
---
--- harpoon:setup()
---
--- vim.keymap.set("n", "<leader>a",function() harpoon:list():add() end)
--- vim.keymap.set("n", "<C-e>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
--- vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
---
--- vim.keymap.set("n", "<C-k>", function() harpoon:list():prev() end)
--- vim.keymap.set("n", "<C-j>", function() harpoon:list():next() end)
-
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local harpoon = require("harpoon")
+
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local make_entry = require("telescope.make_entry")
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+            entry_maker = make_entry.gen_from_file(),
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+    local keys = {
+      {
+        "<leader>ah",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Add current file to Harpoon",
+      },
+      {
+        "<leader>he",
+        function()
+          -- toggle_telescope(harpoon:list())
+          harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "rounded" })
+        end,
+        desc = "Edit Harpoon menu",
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Select Harpoon file " .. i,
+      })
+    end
+    return keys
+  end,
 }
- 
