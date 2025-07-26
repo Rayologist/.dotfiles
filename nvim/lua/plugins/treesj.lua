abe13cfc30ad28d7edf282e8212130cf82001d80
Bowei Chen <bwchen.dev@gmail.com>
2025-07-26 23:57:31 +0800

    Update Treesj plugin configuration: change plugin string format, modify keybindings for toggle and split actions, and ensure proper syntax formatting.

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,12 +1,12 @@
 return {
-  'Wansmer/treesj',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  "Wansmer/treesj",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
   opts = {
     use_default_keymaps = false,
   },
   keys = {
     {
-      "<leader>m",
+      "<leader>js",
       "<cmd>TSJToggle<CR>",
       desc = "Toggle Join/Split",
     },
@@ -19,6 +19,6 @@ return {
       "<leader>sp",
       "<cmd>TSJSplit<CR>",
       desc = "Split",
-    }
-  }
+    },
+  },
 }
