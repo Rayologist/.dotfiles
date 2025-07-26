ed42884c75deec3ad45aec7cb0a009e3b5921fd6
Bowei Chen <bwchen.dev@gmail.com>
2025-07-26 23:57:59 +0800

    Enhance formatter configuration: add additional formatters for markdown, go, and python, and introduce conditions for markdown-toc and markdownlint-cli2 formatters.

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -13,12 +13,18 @@ return {
       html = { "prettier" },
       json = { "prettier" },
       yaml = { "prettier" },
-      markdown = { "prettier" },
+      markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
       graphql = { "prettier" },
       liquid = { "prettier" },
       lua = { "stylua" },
-      go = { "gofmt" },
-      python = { "isort", "black" },
+      sh = { "shfmt", "shellcheck" },
+      go = { "goimports", "gofumpt" },
+      python = {
+        "ruff_fix",
+        "ruff_format",
+        "ruff_organize_imports",
+      },
     },
     format_on_save = {
       lsp_format = "fallback",
@@ -29,6 +35,23 @@ return {
     ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
     formatters = {
       injected = { options = { ignore_errors = true } },
+      ["markdown-toc"] = {
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find("<!%-%- toc %-%->") then
+              return true
+            end
+          end
+        end,
+      },
+      ["markdownlint-cli2"] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d)
+            return d.source == "markdownlint"
+          end, vim.diagnostic.get(ctx.buf))
+          return #diag > 0
+        end,
+      },
     },
   },
   keys = {
